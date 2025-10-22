968333e94dcabfffffc235def33f3089e3fab896
Mahboub Firas <mahboubfiras0@gmail.com>
2025-10-22 14:47:25 -0400

    fixing backend and dockerfile

--- e-commerce-backend/src/main/java/com/example/ecommercebackend/security/WebSecurityConfig.java
+++ e-commerce-backend/src/main/java/com/example/ecommercebackend/security/WebSecurityConfig.java
@@ -66,21 +66,24 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
-                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth ->
-                                auth
-                                        .requestMatchers("/h2-console/**").permitAll()
-                                	.requestMatchers("/api/h2-console/**").permitAll()
-                                	.requestMatchers("/api/auth/**").permitAll()
-                                        .requestMatchers("/auth/**").permitAll()
-                                        .requestMatchers("/v3/api-docs/**").permitAll()
-                                        .requestMatchers("/swagger-ui/**").permitAll()
-                                        .requestMatchers("/images/**").permitAll()
-                                        .anyRequest().authenticated()
-                );
+//                .cors(Customizer.withDefaults())
+//                .csrf(csrf -> csrf.disable())
+//                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(auth ->
+//                                auth
+//                                        .requestMatchers("/h2-console/**").permitAll()
+//                                	.requestMatchers("/api/h2-console/**").permitAll()
+//                                	.requestMatchers("/api/auth/**").permitAll()
+//                                        .requestMatchers("/auth/**").permitAll()
+//                                        .requestMatchers("/v3/api-docs/**").permitAll()
+//                                        .requestMatchers("/swagger-ui/**").permitAll()
+//                                        .requestMatchers("/images/**").permitAll()
+//                                        .anyRequest().authenticated()
+//                );
+	    .csrf().disable()
+	    .authorizeHttpRequests()
+	    .anyRequest().permitAll();
         http.headers(headers ->
                 headers.
                         frameOptions(frameOptionsConfig ->
